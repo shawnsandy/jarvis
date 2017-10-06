b187a4683f37f344393682cceaa240996387dc30
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-06 08:16:22 -0400

    update : import base theme

--- src/resources/views/install/index.blade.php
+++ src/resources/views/install/index.blade.php
@@ -1,4 +1,5 @@
-@extends('jarvis::install.layout') @section('content')
+@extends('jarvis::install.layout')
+@section('content')
 
 <div class="section">
     <div class="column is-8 is-offset-2">
@@ -10,20 +11,21 @@
 
             <div class="container-fluid">
 
+
                 <p class="subtitle is-2 headliners is-uppercase">
                     <i class="fa fa-quote-left"></i> Whoops! It appears that we have not been formally introduced, lets fix
                     that.
                 </p>
-
-                <div class="content is-medium">
+<hr>
+                <div class="content">
 
                     <p class="">
-                        Hi I'm <span class="is-uppercase headliners">{{ config("jarvis.name") }} V{{ config("jarvis.version") }}</span>                        , a simple and smart way to create, package and manage your laravel views (themes) built with the
-                        <a href="https://github.com/shawnsandy/jarvis-toolkit" class="is-capitalized oswald">Jarvis toolkit,</a>                        thats me :) yay. Now that we are buddies lets get started.
+                        Hi I'm <span class="is-uppercase headliners">{{ config("jarvis.name") }} V{{ config("jarvis.version") }}</span>                        , a simple and smart way to create, package and manage laravel views (themes) built with the
+                        <a href="https://github.com/shawnsandy/jarvis-toolkit" class="headliners">Jarvis toolkit,</a>                        thats me :) yay. Now that we are buddies lets get started.
                     </p>
 
                     <p>
-                        I will try to make this as simple as possible, however if you run into hiccups please feel free to read the
+                        I have tried to make this as simple as possible, however if you run into hiccups please feel free to read the
                         <a
                             href="/" class "docs-button">documentation</a> I have found it very helpful at times. Please hit <a href="/" class="start-button">lets get started</a>                            an we shall be on our way. </p>
                     <p>
