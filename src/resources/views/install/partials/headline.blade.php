c163a5d5427bc824946611288e4475ac6547b795
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-06 08:19:43 -0400

    layout : add view headline.blade.php

---
+++ src/resources/views/install/partials/headline.blade.php
@@ -0,0 +1,6 @@
+<div class="headliners is-uppercase subtitle is-2 has-text-centered">
+    <span class="icon"><i class="ion-grid"></i></span>
+    <span class="">
+                                        {{ config("jarvis.name") }} <span class="is-size-4">V{{ config("jarvis.version") }}</span>
+    </span>
+</div>
