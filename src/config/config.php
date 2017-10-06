29b405de54fdaf679fe94683008d35a21b17351a
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-06 08:15:10 -0400

    update : options parameters and add second option

--- src/config/config.php
+++ src/config/config.php
@@ -3,15 +3,32 @@
 return [
     "name" => "Jarvis",
     "description" => "A simple theme management toolkit",
-    "version" => "1.0",
-    "theme" =>
-        [ "default" => [
+    "version" => "0.1[alpha]",
+    "themes" => [
+
+        "default" => [
         "author" => "Shawn Sandy",
         "email" => "shawnsandy04@gmail.com",
-        "website" => "theme website",
+        "website" => "http://",
+        "name" => "Jarvis",
+        "view_path" => "jarvis",
+        "description" => "Default theme",
         "options" => [],
         "fields" => []
-        ]
+        ],
+
+        "galbo" => [
+        "author" => "Shawn Sandy",
+        "email" => "shawnsandy04@gmail.com",
+        "website" => "http://",
+        "name" => "Galbo Realtors",
+        "description" => "Sample 1",
+        "prefix" => "jarvis",
+        "options" => [],
+
+        "fields" => []
+        ],
+
     ]
 
 ];
