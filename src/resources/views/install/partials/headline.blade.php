19741042ee00d821258644451b281e67de6fe19b
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-06 17:00:33 -0400

    update - removed /rearranged selectors

--- src/resources/views/install/partials/headline.blade.php
+++ src/resources/views/install/partials/headline.blade.php
@@ -1,6 +1,9 @@
 <div class="headliners is-uppercase subtitle is-2 has-text-centered">
-    <span class="icon"><i class="ion-grid"></i></span>
-    <span class="">
-                                        {{ config("jarvis.name") }} <span class="is-size-4">V{{ config("jarvis.version") }}</span>
+    <span class="icon">
+    <i class="ion-grid"></i>
     </span>
+    {{ config("jarvis.name") }}
+
+    <span class="is-size-4">V{{ config("jarvis.version") }}</span>
+
 </div>
