f344a9dc9dc763db5f0f412c8f35fc47d4a09071
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-07 20:35:13 -0400

    feature : add controller to handle publish assess

---
+++ src/Controllers/Publish.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ShawnSandy\Jarvis\Controllers;
+
+use ShawnSandy\Jarvis\JarvisController;
+use Illuminate\Support\Facades\Artisan as Artisan;
+
+class Publish extends JarvisController {
+
+     public function __invoke() {
+
+        return "Install files";
+
+     }
+
+
+}
