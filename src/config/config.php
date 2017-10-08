96e3855ab7a803009214ab02e3f556e0f4016cc1
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-08 18:32:40 -0400

    update : add view key to the  config

--- src/config/config.php
+++ src/config/config.php
@@ -5,6 +5,7 @@ return [
     "description" => "A simple theme management toolkit",
     "version" => "0.1(alpha)",
     "theme_admin_key" => env("THEME_ADMIN_KEY", "me@acme.com"),
+    "view" => env("THEME_VIEW", "jarvis"),
     "themes" => [
 
         "default" => [
@@ -12,7 +13,7 @@ return [
         "email" => "me@acme.com",
         "website" => "http://",
         "name" => "Jarvis",
-        "view_path" => "jarvis",
+        "view" => "jarvis",
         "description" => "Default theme",
         "options" => [],
         "fields" => []
