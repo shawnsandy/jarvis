18b8ccc0363a7a3260744b8016d4c7a76657b635
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-07 20:32:13 -0400

    update : add theme_admin_key and dummy email address

--- src/config/config.php
+++ src/config/config.php
@@ -4,11 +4,12 @@ return [
     "name" => "Jarvis",
     "description" => "A simple theme management toolkit",
     "version" => "0.1(alpha)",
+    "theme_admin_key" => env("THEME_ADMIN_KEY", "me@acme.com"),
     "themes" => [
 
         "default" => [
         "author" => "Shawn Sandy",
-        "email" => "shawnsandy04@gmail.com",
+        "email" => "me@acme.com",
         "website" => "http://",
         "name" => "Jarvis",
         "view_path" => "jarvis",
@@ -19,7 +20,7 @@ return [
 
         "galbo" => [
         "author" => "Shawn Sandy",
-        "email" => "shawnsandy04@gmail.com",
+        "email" => "me@acme.com",
         "website" => "http://",
         "name" => "Galbo Realtors",
         "description" => "Sample 1",
