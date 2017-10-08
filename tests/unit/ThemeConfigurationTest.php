b0a5f7dbab77762c7871e9a4a4209f6b382bee9b
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-07 21:31:05 -0400

    test : add check if them.config is array, add setting config to themes

--- tests/unit/ThemeConfigurationTest.php
+++ tests/unit/ThemeConfigurationTest.php
@@ -1,4 +1,4 @@
-<?php namespace ShawnSandy\Jarvis\Test  ;
+<?php namespace ShawnSandy\Jarvis\Test;
 
 
 
@@ -7,12 +7,54 @@ class ThemeConfigurationTest extends TestCase
 {
 
 
-/**
+    /**
      * Test that true does in fact equal true
      */
     public function testTrueIsTrue()
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function check_if_themes_config_is_array()
+    {
+
+        $arr = config("jarvis.themes");
+
+        $this->assertTrue(is_array($arr));
+
+
+    }
+
+    /**
+     * @test
+     */
+    public function can_add_settings_to_themes_array()
+    {
+
+        $config = config("jarvis.themes");
+        $add_config = config(["jarvis.themes.acme" => [
+                "author" => "Daffy Duck",
+                "email" => "me@acme.com",
+                "website" => "http://",
+                "name" => "Galbo Realtors",
+                "description" => "Sample 1",
+                "prefix" => "jarvis",
+                "options" => [],
+
+                "fields" => []
+            ]
+
+        ]);
+
+        $new_config = config("jarvis.themes");
+
+        $this->assertEquals(count($config) + 1, count($new_config));
+
+        $this->assertArrayHasKey("acme", $new_config);
+
     }
 
 
