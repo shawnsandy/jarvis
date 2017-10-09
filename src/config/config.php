b83b23587d1ae344e7a7f94ea38f6609e3bc4555
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-09 18:23:25 -0400

    update - remove default theme_admin_key

--- src/config/config.php
+++ src/config/config.php
@@ -4,7 +4,7 @@ return [
     "name" => "Jarvis",
     "description" => "A simple theme management toolkit",
     "version" => "0.1(alpha)",
-    "theme_admin_key" => env("THEME_ADMIN_KEY", "me@acme.com"),
+    "theme_admin_key" => env("THEME_ADMIN_KEY", null),
     "view" => env("THEME_VIEW", "jarvis"),
     "base_url" => "jarvis",
     "themes" => [
