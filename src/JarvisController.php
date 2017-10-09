3825541c2ccc9eb57d44167c88ae75c497ce4c6e
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-09 12:31:31 -0400

    update : default path to readme

--- src/JarvisController.php
+++ src/JarvisController.php
@@ -8,12 +8,14 @@ class JarvisController extends Controller
 
     public $views;
     public $resources;
+    public $read_me;
 
 
     public function __construct() {
 
         $this->views = __DIR__."/resources/views";
         $this->resources = __DIR__."/resources";
+        $this->resources = __DIR__."../README.md";
 
     }
 
