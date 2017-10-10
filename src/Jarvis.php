57fc5003811f2cb225c221479c1cad3b43c81333
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-10 17:11:28 -0400

    Feature : convert the env file to array

--- src/Jarvis.php
+++ src/Jarvis.php
@@ -1,8 +1,9 @@
 <?php
 
 
+
 /**
-Created by PhpStorm.
+reated by PhpStorm.
      * User: Shawn
      * Date: 2/22/2017
      * Time: 10:03 PM
@@ -18,44 +19,47 @@ class Jarvis
 
 
 
+
 	/**
 	* App routes
-		         *
-		         * @return void
-		         */
-		        public function routes(){
+			         *
+			         * @return void
+			         */
+			        public function routes(){
 		require  __DIR__.'/routes.php';
 	}
 
 
 
 
+
 	/**
 	* Install routes
-		         *
-		         * @return void
-		         */
-		        public function install_routes(){
+			         *
+			         * @return void
+			         */
+			        public function install_routes(){
 		require  __DIR__.'/routes/theme-install.php';
 	}
 
 
 
 
+
 	/**
 	* Load views
-	*
-	* @param string $blade_path path to your blade file `partials.example`.
-	* @return void
-	*/
-        public function views($blade_path) {
-            $view = config("jarvis.view")."::";
+		*
+		* @param string $blade_path path to your blade file `partials.example`.
+		* @return void
+		*/
+	        public function views($blade_path) {
+		$view = config("jarvis.view")."::";
 
-            if(is_null(config("jarvis.view")))
-            $view = null;
+		if(is_null(config("jarvis.view")))
+		            $view = null;
 
-            return $view.$blade_path;
-        }
+		return $view.$blade_path;
+	}
 
 	public function url($route_url) {
 
@@ -73,6 +77,27 @@ class Jarvis
 		return $md;
 
 
+	}
+
+	public function env_to_array($file = null)
+	    {
+            if(is_null($file))
+            $file = __DIR__."./../.env.example";
+
+		$string = file_get_contents($file);
+		$string = preg_split('/\n+/', $string);
+		$returnArray = array();
+		foreach($string as $one){
+			if (preg_match('/^(#\s)/', $one) === 1) {
+				continue;
+			}
+			$entry = explode("=", $one, 2);
+			$returnArray[$entry[0]] = isset($entry[1]) ? $entry[1] : null;
+		}
+		return array_filter($returnArray,function($key) {
+			return !empty($key);
+		}
+		,ARRAY_FILTER_USE_KEY);
 	}
 
 
