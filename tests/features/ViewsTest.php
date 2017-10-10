66c069847009436fbb5e0efdb6e20e4c80077771
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-10 17:14:12 -0400

    test : end to array conversion

--- tests/features/ViewsTest.php
+++ tests/features/ViewsTest.php
@@ -2,7 +2,7 @@
 
 namespace ShawnSandy\Jarvis\Test;
 
-use ShawnSandy\Jarvis\JarvisFacade as Jarvis;
+use Facades\ShawnSandy\Jarvis\Jarvis;
 
 class ViewsTest Extends TestCase
 
@@ -28,6 +28,19 @@ class ViewsTest Extends TestCase
         $this->assertEquals("jarvis::index", $default_view);
 
         $this->assertEquals("default::index", $dynamic_view);
+
+    }
+
+    /** @test */
+    public function can_convert_env_file_to_array() {
+
+        $env_array = Jarvis::env_to_array();
+
+        $this->assertTrue(count($env_array) > 1);
+
+        $this->assertTrue(is_array($env_array));
+
+        $this->assertTrue($env_array["APP_NAME"] == "Laravel");
 
     }
 
