0d944adb0c8f2c8526a40cf0dfd91984a523864a
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-11 21:40:02 -0400

    layout : add asides page views

---
+++ src/resources/views/layouts/asides.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>
+
+
+
+    </title>
+
+@include(jarvis_views("partials.asides-header"))
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- Generate using http://realfavicongenerator.net/ -->
+
+
+
+</head>
+
+<body class="asides">
+
+
+    <div class="page">
+
+        <div class="columns is-marginless">
+
+
+            <div class="column is-one-quarter is-light is-paddingless">
+                <div class="side-nav">
+                    <div class="hero is-fullheight">
+
+                        <div class="hero-head">
+                            <div class="section">
+                                <h2 class="subtitle is-2 has-text-centered is-light">
+                                    Asides
+                                </h2>
+                            </div>
+                        </div>
+
+                        <div class="hero-body">
+                            <div class="content">
+
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi ab veniam distinctio
+                                    perspiciatis repudiandae deserunt facilis. Quidem necessitatibus officiis placeat, eos
+                                    itaque cumque dolor dolorem impedit nihil optio sint quaerat quae temporibus corporis
+                                    ratione voluptatem animi quam quasi sunt quisquam.</p>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="column is-paddingless">
+
+                <!-- content -->
+                @yield("content")
+                <!-- endcontent -->
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    <script src="/jarvis/js/app-40a6ab64e2c8ef467e4c.js"></script>
+
+
+
+</body>
+
+</html>
