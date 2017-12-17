1dbe79a32b4e0ec40b821a04e4f1b48e3d95edf0
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-17 05:56:49 -0500

    Layout : move roles to the side
    tweaking the layout

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -85,7 +85,9 @@
                 <div>
                 <div class="card-content">
                 <p class="subtitle is-3"><i class="im im-users"></i> User Roles</p>
-
+                @component("dashauth::partials.roles", [ "user" => Auth::user() ])
+                @slot('btn_class')  button is-info is-outlined is-uppercase  @endslot
+                @endcomponent
                 </div>
 
                 {{--  <div class="card-content">
@@ -121,6 +123,10 @@
 
 		<div class="column is-3 sidebar-right">
 			<div class="cards">
+
+
+
+
 	<div class="card-content">
 		<div class="header">
 			<div class="subtitle is-3">
@@ -128,6 +134,10 @@
 			 Dashboard Sidebar
 			 </div>
 		</div>
+        <p class="subtitle is-3">
+                @component("dashauth::partials.roles", [ "user" => Auth::user() ])
+                @slot('btn_class')  button is-info is-outlined is-uppercase  @endslot
+                @endcomponent
 		<p class="class">
 			Quia ex et reprehenderit cum sit maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam temporibus deleniti
 			ex ut sint eveniet dicta similique aut neque unde dolorum nobis, quidem blanditiis impedit reiciendis consectetur eligendi
