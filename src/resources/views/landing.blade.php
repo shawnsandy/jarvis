f0f223430ba383aa4f0385b4f147bd3b0e755586
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-17 11:51:48 -0500

    layout : add landing page
    Landing suited for apps / digital products

---
+++ src/resources/views/landing.blade.php
@@ -0,0 +1,239 @@
+@php
+$theme_class = "front-page";
+@endphp
+@extends(jarvis_views('layouts.theme'))
+@section('content')
+@include(jarvis_views('components.landing-cover'))
+
+<section class="actions section is-medium">
+
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <h1 class="subtitle is-2 has-text-centered">
+                    Jarvis is a simply smart way to create and
+        manage Laravel views (themes).
+                </h1>
+
+            </div>
+
+        </div>
+    </div>
+
+</section>
+
+
+<section class="section is-medium featured">
+
+    <div class="container">
+
+        <div class="container">
+
+            <div class="columns is-multiline">
+
+                <div class="column is-4">
+                    <div class="box">
+                    <p class="subtitle is-4">Title</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, asperiores?</p>
+                    </div>
+                </div>
+
+                <div class="column is-4">
+
+                    <div class="box">
+                    <p class="subtitle is-4">Title</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, asperiores?</p>
+                    </div>
+                </div>
+
+                <div class="column is-4">
+
+                    <div class="box">
+                    <p class="subtitle is-4">Title</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, asperiores?</p>
+                    </div>
+                </div>
+
+                <div class="column is-4">
+                    <div class="box">
+                    <p class="subtitle is-4">Title</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, asperiores?</p>
+                    </div>
+                </div>
+
+                <div class="column is-4">
+
+                    <div class="box">
+                    <p class="subtitle is-4">Title</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, asperiores?</p>
+                    </div>
+                </div>
+
+                <div class="column is-4">
+
+                    <div class="box">
+                    <p class="subtitle is-4">Title</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, asperiores?</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+</section>
+
+<main class="section is-medium content">
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-6">
+                <h2 data-module="example" class="article article-h1">
+                    Blendid!
+                </h2>
+                <p class="">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur assumenda quaerat quam possimus laudantium
+                    perferendis cum, iusto debitis delectus molestiae officiis atque, consequuntur! Temporibus saepe quis
+                    aliquam non. Perspiciatis!
+                </p>
+                <p>
+                    <a href="" class="button">Read More</a>
+                </p>
+            </div>
+
+            <div class="column is-6">
+                <h2 data-module="example" class="article article-h1">
+                    Blendid!
+                </h2>
+                <p class="">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur assumenda quaerat quam possimus laudantium
+                    perferendis cum, iusto debitis delectus molestiae officiis atque, consequuntur! Temporibus saepe quis
+                    aliquam non. Perspiciatis!
+                </p>
+                <p>
+                    <a href="" class="button">Read More</a>
+                </p>
+            </div>
+
+            <div class="column is-6">
+                <h2 data-module="example" class="article article-h1">
+                    Blendid!
+                </h2>
+                <p class="">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur assumenda quaerat quam possimus laudantium
+                    perferendis cum, iusto debitis delectus molestiae officiis atque, consequuntur! Temporibus saepe quis
+                    aliquam non. Perspiciatis!
+                </p>
+                <p>
+                    <a href="" class="button">Read More</a>
+                </p>
+            </div>
+
+            <div class="column is-6">
+                <h2 data-module="example" class="article article-h1">
+                    Blendid!
+                </h2>
+                <p class="">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur assumenda quaerat quam possimus laudantium
+                    perferendis cum, iusto debitis delectus molestiae officiis atque, consequuntur! Temporibus saepe quis
+                    aliquam non. Perspiciatis!
+                </p>
+                <p>
+                    <a href="" class="button">Read More</a>
+                </p>
+            </div>
+
+        </div>
+    </div>
+</main>
+<hr>
+<section class="section">
+    <div class="container">
+
+        <h2 class="title is-2">Collections</h2>
+
+        <div class="columns">
+
+            <div class="column">
+
+                <div class="collection">
+                    <p class="">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, porro?
+                    </p>
+                </div>
+                <div class="collection">
+                    <p class="">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, porro?
+                    </p>
+                </div>
+                <div class="collection">
+                    <p class="">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, porro?
+                    </p>
+                </div>
+                <div class="collection">
+                    <p class="">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, porro?
+                    </p>
+                </div>
+                <div class="collection">
+                    <p class="">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, porro?
+                    </p>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="section is-medium">
+    <div class="container">
+        <div class="columns">
+
+            <div class="column">
+                <div class="cards">
+                    <div class="card-content">
+                        <p class="class">
+                            Quia ex et reprehenderit cum sit maxime.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="column">
+                <div class="cards">
+                    <div class="card-content">
+                        <p class="class">
+                            Quia ex et reprehenderit cum sit maxime.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+            <div class="column">
+                <div class="cards">
+                    <div class="card-content">
+                        <p class="class">
+                            Quia ex et reprehenderit cum sit maxime.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+@endsection
